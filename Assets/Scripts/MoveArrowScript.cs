24ba636038f3eed5dbdae022533cb5642b32a98f
RMSebastian <105923386+RMSebastian@users.noreply.github.com>
2023-06-01 14:18:08 -0300

    Rescritura de Move Arrow Script

--- Assets/Scripts/MoveArrowScript.cs
+++ Assets/Scripts/MoveArrowScript.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class MoveArrowScript : MonoBehaviour
 {
+    public Animator anim;
 
     public LevelManager.SceneNames SceneToMoveTo;
     private LevelManager levelManager;
     private void Start()
     {
+        anim = GetComponent<Animator>();
         levelManager = GameManager.Instance.levelManager;
     }
     public void GoToScene()
     {
         print("is executing");
+        anim.SetTrigger("Moving");
+    }
+    public void MovingToNextScene()
+    {
         GameManager.Instance.onlyCamera.MoveCameraPositionTo(levelManager.GetCameraNewPosition(SceneToMoveTo.GetHashCode()));
     }
 
