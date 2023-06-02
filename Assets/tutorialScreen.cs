873fe87d611a7f6b3cc793e4ac5d4289aacec97d
RMSebastian <105923386+RMSebastian@users.noreply.github.com>
2023-06-02 13:17:33 -0300

    Tutorial prefab

---
+++ Assets/tutorialScreen.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorialScreen : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+}
