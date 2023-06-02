7352a2d7e9498d90403e107fb96b10f649becf27
Estilazo <estilazofreestyle@gmail.com>
2023-06-02 01:04:19 -0300

    commit para ver el pull

--- Assets/Scripts/PointAndClickSystem.cs
+++ Assets/Scripts/PointAndClickSystem.cs
@@ -9,6 +9,8 @@ public class PointAndClickSystem : MonoBehaviour
     public Sprite[] everyTrashSprite;
     private Sprite[] thisTrashSprites;
     public TypeOfTrash thisTypeOfTrash;
+    private AudioSource touchEmit;
+    public AudioClip[] touchSound;
     
     public enum TypeOfTrash
     {
@@ -34,7 +36,7 @@ public class PointAndClickSystem : MonoBehaviour
     private void Awake()
     {
         thisTrashSprites = new Sprite[2];
-
+        touchEmit = GetComponent<AudioSource>();
         sr = GetComponent<SpriteRenderer>();
 
         switch (thisTypeOfTrash)
@@ -42,30 +44,37 @@ public class PointAndClickSystem : MonoBehaviour
             case TypeOfTrash.vaso:
                 thisTrashSprites[0] = everyTrashSprite[0];
                 thisTrashSprites[1] = everyTrashSprite[1];
+                touchEmit.clip = touchSound[0];
                 break;
             case TypeOfTrash.lata:
                 thisTrashSprites[0] = everyTrashSprite[2];
                 thisTrashSprites[1] = everyTrashSprite[3];
+                touchEmit.clip = touchSound[1];
                 break;
             case TypeOfTrash.botella:
                 thisTrashSprites[0] = everyTrashSprite[4];
                 thisTrashSprites[1] = everyTrashSprite[5];
+                touchEmit.clip = touchSound[0];
                 break;
             case TypeOfTrash.botellaAbollada:
                 thisTrashSprites[0] = everyTrashSprite[6];
                 thisTrashSprites[1] = everyTrashSprite[7];
+                touchEmit.clip = touchSound[0];
                 break;
             case TypeOfTrash.lataAplastada:
                 thisTrashSprites[0] = everyTrashSprite[8];
                 thisTrashSprites[1] = everyTrashSprite[9];
+                touchEmit.clip = touchSound[1];
                 break;
             case TypeOfTrash.bolsa:
                 thisTrashSprites[0] = everyTrashSprite[10];
                 thisTrashSprites[1] = everyTrashSprite[11];
+                touchEmit.clip = touchSound[2];
                 break;
             case TypeOfTrash.bolsaAplastada:
                 thisTrashSprites[0] = everyTrashSprite[12];
                 thisTrashSprites[1] = everyTrashSprite[13];
+                touchEmit.clip = touchSound[2];
                 break;
         }
         sr.sprite = thisTrashSprites[0];
@@ -99,6 +108,7 @@ public class PointAndClickSystem : MonoBehaviour
     {
         if (!isClicking)
         {
+            touchEmit.Play();
             isClicking = true;
             sr.sprite = thisTrashSprites[0];
            initialPosition = transform.position;
