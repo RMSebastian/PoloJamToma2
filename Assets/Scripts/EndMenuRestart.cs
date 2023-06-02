e0e04c8c512f41035e7ad101cf712de0f61ebc38
RMSebastian <105923386+RMSebastian@users.noreply.github.com>
2023-06-02 00:44:40 -0300

    Cambios de las camaras de las escenas Main y End

--- Assets/Scripts/EndMenuRestart.cs
+++ Assets/Scripts/EndMenuRestart.cs
@@ -8,6 +8,9 @@ public class EndMenuRestart : MonoBehaviour
     // Start is called before the first frame update
     void Restart()
     {
+        Destroy(GameManager.Instance);
+        Destroy(MusicManagerMusic.Instance);
+        Destroy(InventorySystem.Instance);
         SceneManager.LoadScene("MainMenu");
     }
 }
